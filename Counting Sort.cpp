260a0ea35262bdce816689913097665638109a05
Giovanni Bruno <30694919+GiovanniBru@users.noreply.github.com>
2018-09-06 13:11:56 -0300

    Rename Counting Sort.txt to Counting Sort.cpp

--- Counting Sort.cpp
+++ Counting Sort.cpp
@@ -1,39 +1,39 @@
-/* Counting Sort algoritmo de ordenaÁ„o pseudo-linear onde n„o h· comparaÁ„o entre os elementos do array a ser ordenado */
+/* Counting Sort algoritmo de ordena√ß√£o pseudo-linear onde n√£o h√° compara√ß√£o entre os elementos do array a ser ordenado */
 
 #include <iostream>
 
 using namespace std;
 
-void CountingSort(int aray[],int tamanho){ // O escopo È o maior elemento somado de 1, para incluir o zero.
+void CountingSort(int aray[],int tamanho){ // O escopo √© o maior elemento somado de 1, para incluir o zero.
 
-    int b[tamanho]; // Array B usado na orndenaÁ„o..
+    int b[tamanho]; // Array B usado na orndena√ß√£o..
 
-    int escopo = aray[0]; // Aqui nesse for eu calculo o escopo, isso È necess·rio pois vai me dar o tamanho do array C.. eu somo 1 no final para contemplar o 0.
+    int escopo = aray[0]; // Aqui nesse for eu calculo o escopo, isso √© necess√°rio pois vai me dar o tamanho do array C.. eu somo 1 no final para contemplar o 0.
     for (int i = 1; i < tamanho; i++)
         if (aray[i] > escopo)
             escopo = aray[i]+1;
 
     int c[escopo]={0}; // Cria o C dependendo dos elementos do array de entrada e o inicia tudo com zero;
 
-    for(int i=0;i<tamanho;i++) { // Contagem da frequÍncia,
+    for(int i=0;i<tamanho;i++) { // Contagem da frequ√™ncia,
 
         ++c[aray[i]]; // o c[] vai receber como indice um elemento do array de entrada..
     }
 
-    for(int i=1;i<escopo;i++) { // Faz o somatÛrio das partes l· no C, podemos considerar que o numero representa quantos algarismos temos a esquerda do numero..
+    for(int i=1;i<escopo;i++) { // Faz o somat√≥rio das partes l√° no C, podemos considerar que o numero representa quantos algarismos temos a esquerda do numero..
 
     c[i]+=c[i-1];
 
     }
 
-    for(int i=tamanho-1;i>=0;i--){ // Percore do final pro final, para garantir que o algoritmo È est·vel.
+    for(int i=tamanho-1;i>=0;i--){ // Percore do final pro final, para garantir que o algoritmo √© est√°vel.
 
         b[c[aray[i]]-1]=aray[i]; // O C vai determinar em qual local o aray[i] vai ser colocado no array de suporte, B.
         --c[aray[i]];
     }
 
     for(int i=0;i<tamanho;i++) {
-    aray[i]=b[i]; // Joga os valores de b que j· est„o ordenados no array de foi entrado..
+    aray[i]=b[i]; // Joga os valores de b que j√° est√£o ordenados no array de foi entrado..
     }
 
 
